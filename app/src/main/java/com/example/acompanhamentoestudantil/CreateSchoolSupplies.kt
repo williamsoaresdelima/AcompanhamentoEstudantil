d44237f0800798064871abf88712b7d005094a3c
wlima <wlima@82Y34G3>
2023-03-07 10:38:13 -0300

    login google

--- app/src/main/java/com/example/acompanhamentoestudantil/CreateSchoolSupplies.kt
+++ app/src/main/java/com/example/acompanhamentoestudantil/CreateSchoolSupplies.kt
@@ -3,32 +3,36 @@ package com.example.acompanhamentoestudantil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseBooleanArray
 import android.view.View
 import android.widget.ArrayAdapter
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class CreateSchoolSupplies : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences;
+    private var gson = Gson();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_school_supplies)
         supportActionBar?.hide()
 
-        var listView = findViewById<android.widget.ListView>(R.id.list)
-        var editText = findViewById<android.widget.EditText>(R.id.textInput)
+        sharedPreferences = getSharedPreferences("materiais-escolares", Context.MODE_PRIVATE)
 
-        var listAdapter = arrayListOf<String>()
+        val listView = findViewById<android.widget.ListView>(R.id.list)
+        val editText = findViewById<android.widget.EditText>(R.id.textInput)
 
-        var adapter = ArrayAdapter(this,
+        val listAdapter = getData();
+
+        val adapter = ArrayAdapter(this,
             android.R.layout.simple_list_item_multiple_choice,
             listAdapter
         )
 
-        findViewById<android.widget.Button>(R.id.btnAdd).setOnClickListener{
-            listAdapter.add(editText.text.toString())
-            listView.adapter = adapter
-            adapter.notifyDataSetChanged()
-
-            editText.text.clear()
-        }
+        listView.adapter = adapter;
+        adapter.notifyDataSetChanged()
 
         findViewById<View>(R.id.logout).setOnClickListener {
             val activity = Intent(this, LoginScreen::class.java)
@@ -39,5 +43,50 @@ class CreateSchoolSupplies : AppCompatActivity() {
             val activity = Intent(this, Dashboard::class.java)
             startActivity(activity)
         }
+
+        findViewById<android.widget.Button>(R.id.btnAdd).setOnClickListener{
+            listAdapter.add(editText.text.toString())
+            listView.adapter = adapter
+            adapter.notifyDataSetChanged()
+
+            saveData(listAdapter)
+            editText.text.clear()
+        }
+
+        findViewById<View>(R.id.btnDelete).setOnClickListener {
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item = count - 1
+            while (item >= 0) {
+                if (position.get(item)) {
+                    adapter.remove(listAdapter.get(item));
+                }
+                item--;
+            }
+            saveData(listAdapter)
+            position.clear();
+            adapter.notifyDataSetChanged();
+        }
+
+        findViewById<View>(R.id.btnClean).setOnClickListener {
+            listAdapter.clear();
+            saveData(listAdapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private fun getData(): ArrayList<String> {
+        val arrayJson = sharedPreferences.getString("lista", null);
+        return if (arrayJson.isNullOrEmpty()) {
+            arrayListOf<String>();
+        } else {
+            gson.fromJson(arrayJson, object : TypeToken<ArrayList<String>>(){}.type)
+        }
+    }
+    private fun saveData(array: ArrayList<String>) {
+        var arrayJson = gson.toJson(array);
+        var editor = sharedPreferences.edit();
+        editor.putString("lista", arrayJson);
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/acompanhamentoestudantil/LoginScreen.kt
+++ app/src/main/java/com/example/acompanhamentoestudantil/LoginScreen.kt
@@ -4,21 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class LoginScreen : AppCompatActivity() {
-
+    val Req_Code: Int = 123;
+    lateinit var mGoogleSignInClient: GoogleSignInClient;
+    lateinit var  firebaseAuth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
         supportActionBar?.hide()
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         findViewById<View>(R.id.btnLogin).setOnClickListener {
             var emailView = findViewById<android.widget.EditText>(R.id.etEmail)
             var senhaView = findViewById<android.widget.EditText>(R.id.etPassword)
-
+            val activity = Intent(this, Dashboard::class.java)
+            startActivity(activity)
             if(senhaView.text.toString() == "123" && emailView.text.toString() == "teste@gmail.com")
             {
                 val activity = Intent(this, Dashboard::class.java)
@@ -29,6 +50,48 @@ class LoginScreen : AppCompatActivity() {
         findViewById<View>(R.id.btnSignIn).setOnClickListener {
             val activity = Intent(this, CreateAccount::class.java)
             startActivity(activity)
+        }
+
+        findViewById<View>(R.id.btnGoogleSignIn).setOnClickListener {
+            signInGoogle();
+        }
+    }
+
+    private fun signInGoogle(){
+        val signIntent: Intent = mGoogleSignInClient.signInIntent;
+        startActivityForResult(signIntent, Req_Code);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == Req_Code){
+            val result = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleResult(result);
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try{
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java);
+            Toast.makeText(this, "Logado com sucesso", Toast.LENGTH_SHORT).show();
+            if(account != null){
+                UpdateUser(account)
+            }
+        }catch (e: ApiException){
+            println(e)
+            Toast.makeText(this, "Falha ao logar", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private fun UpdateUser(account: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task ->
+            if(task.isSuccessful){
+                val intent = Intent(this, MainActivity::class.java);
+                startActivity(intent);
+                finish()
+            }
         }
     }
 }
